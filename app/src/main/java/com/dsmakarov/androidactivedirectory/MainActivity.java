e82f95afeb52642df6f0090025990a17e51707ec
Fenikz <dsmakarov@gmail.com>
2016-03-30 22:44:57 +0300

    Added getIp method

--- app/src/main/java/com/dsmakarov/androidactivedirectory/MainActivity.java
+++ app/src/main/java/com/dsmakarov/androidactivedirectory/MainActivity.java
@@ -3,11 +3,16 @@ package com.dsmakarov.androidactivedirectory;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -17,16 +22,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: " + ping());
 
+        Log.d(TAG, "onCreate: " + ping("yandex.ru"));
+
+        TextView ipTextView = (TextView) findViewById(R.id.ip_text_view);
+        ipTextView.setText(getIPAddress(true));
     }
 
-    private String ping() {
+    private String ping(String address) {
 
         Process process;
 
         try {
-            process = Runtime.getRuntime().exec("ping -c 1 -w 1 google.com");
+            process = Runtime.getRuntime().exec("ping -c 1 -w 1 " + address);
 
             BufferedReader bufferedReader = new BufferedReader(
                     new InputStreamReader(process.getInputStream()));
@@ -38,41 +46,44 @@ public class MainActivity extends Activity {
             }
 
             process.destroy();
-            Toast.makeText(getApplicationContext(), "Reachable", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), stringBuilder.toString(), Toast.LENGTH_LONG).show();
             return stringBuilder.toString();
 
         } catch (IOException e) {
             e.printStackTrace();
-            return "Not worked";
+            return "";
         }
     }
 
-    /*
-    executeCmd("ping -c 1 -w 1 google.com", false);
-
-    public static String executeCmd(String cmd, boolean sudo){
+    /**
+     * Get IP address from first non-localhost interface
+     * @param ipv4  true=return ipv4, false=return ipv6
+     * @return  address or empty string
+     */
+    public static String getIPAddress(boolean useIPv4) {
         try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
 
-            Process p;
-            if(!sudo)
-                p= Runtime.getRuntime().exec(cmd);
-            else{
-                p= Runtime.getRuntime().exec(new String[]{"su", "-c", cmd});
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
             }
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-            String s;
-            String res = "";
-            while ((s = stdInput.readLine()) != null) {
-                res += s + "\n";
-            }
-            p.destroy();
-            return res;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } catch (Exception ex) { } // for now eat exceptions
         return "";
-
     }
-    */
 }
