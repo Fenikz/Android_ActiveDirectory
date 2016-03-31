bbf39075b883e2a8eca72cb8e1d1b3220809b126
Fenikz <dsmakarov@gmail.com>
2016-03-31 22:06:25 +0300

    Added AsyncTask ScanLocalIpsTask

--- app/src/main/java/com/dsmakarov/androidactivedirectory/MainActivity.java
+++ app/src/main/java/com/dsmakarov/androidactivedirectory/MainActivity.java
@@ -2,7 +2,9 @@ package com.dsmakarov.androidactivedirectory;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -25,8 +27,9 @@ public class MainActivity extends Activity {
 
         //Получаем текущий IP-адресс
         mCurrentIp = NetHelper.getIPAddress(true);
+
         // TODO: 31.03.2016 Запускать в отдельном потоке
-        scanLoaclIps(mCurrentIp);
+        new ScanLocalIpsTask().execute(mCurrentIp);
 
         TextView ipTextView = (TextView) findViewById(R.id.ip_textview);
         ipTextView.setText(mCurrentIp);
@@ -51,36 +54,43 @@ public class MainActivity extends Activity {
         });
     }
 
-    private HashMap<String, String> scanLoaclIps(String currentIp) {
-
-        HashMap<String, String> localIpsHashMap = new HashMap<>();
-
-        //Позиция последней точки в IP-адресе
-        int lastDot = currentIp.lastIndexOf(".");
+    private class ScanLocalIpsTask extends AsyncTask<String, Integer, String> {
 
         //Предполагаемая маска (255.255.255.0)
 
-        //Предполагаемое начало диапазона
-        String subnetIp = currentIp.substring(0, lastDot + 1);
 
-        for (int i = 0; i < 255; i++) {
-            // TODO: 31.03.2016 Добавить ограничение на свой IP
-            if (NetHelper.ping(subnetIp + i).contains("1 received")) {
-                localIpsHashMap.put(subnetIp + i, "Enabled");
-            } else {
-                localIpsHashMap.put(subnetIp + i, "Disabled");
+        @Override
+        protected String doInBackground(String... params) {
+            HashMap<String, String> localIpsHashMap = new HashMap<>();
+
+            Log.d(TAG, "doInBackground: Начало выполнения " + params[0]);
+            
+            //Позиция последней точки в IP-адресе
+            int lastDot = params[0].lastIndexOf(".");
+            String subnetIp = params[0].substring(0, lastDot + 1);
+
+            for (int i = 0; i < 255; i++) {
+                Log.d(TAG, "doInBackground: Цикл " + i);
+                // TODO: 31.03.2016 Добавить ограничение на свой IP
+                if (NetHelper.ping(subnetIp + i).contains("1 received")) {
+                    localIpsHashMap.put(subnetIp + i, "Enabled");
+                } else {
+                    localIpsHashMap.put(subnetIp + i, "Disabled");
+                }
             }
+
+            for (HashMap.Entry<String, String> element : localIpsHashMap.entrySet()) {
+                Log.d(TAG, "scanLoaclIps: IP " + element.getKey() + " is " + element.getValue() );
+            }
+
+            return "OK";
         }
 
-        for (HashMap.Entry<String, String> element : localIpsHashMap.entrySet()) {
-            Log.d(TAG, "scanLoaclIps: IP " + element.getKey() + " is " + element.getValue() );
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            // TODO: 31.03.2016 Добавить в процентах (255 = 100%)
         }
-
-        //Log.d(TAG, "scanLoaclIps: assume mask " + subnetIp);
-
-            //С помщью маски подсети получаем кол-во адрессов для сканирования сети
-
-        return localIpsHashMap;
     }
 
 }
